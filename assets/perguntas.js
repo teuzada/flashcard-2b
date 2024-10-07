0cb580c96a2546f3cca2eb5e2e9081c603f0fb24
teuzada <163007822+teuzada@users.noreply.github.com>
2024-10-07 11:18:42 -0300

    fsdfsdfsdfsdf

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Vasco',
+    'Quem fundo o Vasco?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Batata',
+    'Quem plantou batata em contenda?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido por ser pai do Flamengo?',
+    'O vasco'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil',
+    'Pedrinho matador'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem construiu o estádio do Vasco',
+    'Conserta Felix'
+)
